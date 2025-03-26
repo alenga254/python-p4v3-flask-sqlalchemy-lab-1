cfa8419c839ff4f05b6f04c00179899772e3ccb7
alenga254 <alengaryan@gmail.com>
2025-03-26 10:21:12 +0300

    edited the get_earthquake view to jsonify the response

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -22,13 +22,20 @@ def index():
 
 # Add views here
 @app.route('/earthquakes/<int:id>')
-def earthquake(id):
-    quake = Earthquake.query.filter_by(id=id).first()
-    if quake is None:
-        body = {'message': f'Earthquake {id} not found.'}
-        return make_response(body, 404)
-    body = quake.to_dict()
-    return make_response(body, 200)
+
+@app.route("/earthquakes/<int:id>", methods=["GET"])
+def get_earthquake(id):
+    earthquake = db.session.get(Earthquake, id)
+    
+    if earthquake:
+        return jsonify({
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        }), 200
+    else:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
 
 
 @app.route('/earthquakes/magnitude/<float:magnitude>')
